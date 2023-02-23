2b52b627adc45b62ad2f0ea1476de186dc8de838
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2023-02-24 02:46:00 +0600

    flutter_geocoder

--- lib/convert_latlang_to_address.dart
+++ lib/convert_latlang_to_address.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: unused_local_variable, avoid_print
+
 import 'package:flutter/material.dart';
+import 'package:flutter_geocoder/geocoder.dart';
 import 'package:velocity_x/velocity_x.dart';
+
+
 
 class ConvertLatLangToAddress extends StatefulWidget {
   const ConvertLatLangToAddress({super.key});
@@ -12,16 +17,55 @@ class ConvertLatLangToAddress extends StatefulWidget {
 class _ConvertLatLangToAddressState extends State<ConvertLatLangToAddress> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-       title: "Google Map".text.make(),
-       centerTitle: true,
+        title: "Google Map".text.make(),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        // crossAxisAlignment: CrossAxisAlignment.end,
+        // crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
 
-        children: ["This is demo colum".text.make()],
+
+          GestureDetector(
+            onTap: () async {
+              final coordinates = Coordinates(23.818968, 90.434371);
+              var address = await Geocoder.local.findAddressesFromCoordinates(coordinates);
+
+              var first = address.first;
+              print("Address:  ${first.featureName}${first.addressLine}");
+            },
+          ),
+
+
+
+
+          "Convert"
+              .text
+              .make()
+              .box
+              .color(Colors.red)
+              .padding(Vx.m16)
+              .roundedSM
+              .shadowMax
+              .make()
+              .centered(),
+          //   const  VxResponsive(
+          //   xsmall: Text("Hi d Small"),
+          //   small: Text("Hi Small"),
+          //   medium: Text("Hi Medium"),
+          //   large: Text("Hi Large"),
+          //   xlarge: Text("Hi Extra Large"),
+          //   fallback: Text("Hi Nothing Specified"),
+          // ),
+          // 20.heightBox,
+
+
+
+
+
+        ],
       ),
     );
   }
